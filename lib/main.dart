d340cefc382ee082d5b2e5bc3f6e8a21168c27e9
Carlos Huanca <202000203c@utea.edu.pe>
2022-12-11 17:00:19 -0500

    commit video 5-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firetask/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
