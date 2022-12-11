7c10c040defe753506d3124880c7557ad8ded41d
Carlos Huanca <202000203c@utea.edu.pe>
2022-12-11 13:11:13 -0500

    commit video del 1-4

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firetask/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+   title: "Firetask",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
